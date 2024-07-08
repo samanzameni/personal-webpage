8f0b81e7da3421bdfb8e63129bb855f70e14f716
Saman Zameni <saman.zameni@gmail.com>
2024-07-08 23:32:14 +0330

    fixing education page bugs.

--- src/app/feature/components/navbar/navbar.component.ts
+++ src/app/feature/components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [
-    RouterLink
-  ],
+    imports: [
+        RouterLink,
+        RouterLinkActive
+    ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
